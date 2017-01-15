8220f7d1e9c2a6296fec2a2c0f98adff8cbb1d07
Feliciano Santana <fsfelicianosantana@gmail.com>
2017-01-15 09:45:07 -0300

    Update crud components to call reducers

--- test/components/client-crud.test.js
+++ test/components/client-crud.test.js
@@ -12,24 +12,49 @@ const schema = {
 };
 
 describe("ClientCrud", () => {
+    let store;
+    let component;
+    let player;
+
+    beforeEach(() => {
+        store = {
+            dispatch: spy()
+        };
+        component = new ClientCrud(schema, store);
+        player = {
+            id: 208,
+            x: 9211,
+            y: 2101
+        };
+    });
+
     it("should listen for SR_CRUD:NETWORK_<schema>_CREATED", () => {
-        const component = new ClientCrud(schema);
         const handler = spy(component._eventListeners, "SR_CRUD:NETWORK_PLAYER_CREATED");
-        component.fire({type: "SR_CRUD:NETWORK_PLAYER_CREATED"});
+        component.fire({type: "SR_CRUD:NETWORK_PLAYER_CREATED", player});
         expect(handler).to.have.been.called;
+        expect(store.dispatch).to.have.been.calledWith({
+            type: "SR_CRUD:CREATE_PLAYER",
+            player 
+        });
     });
 
     it("should listen for SR_CRUD:NETWORK_<schema>_UPDATED", () => {
-        const component = new ClientCrud(schema);
         const handler = spy(component._eventListeners, "SR_CRUD:NETWORK_PLAYER_UPDATED");
-        component.fire({type: "SR_CRUD:NETWORK_PLAYER_UPDATED"});
+        component.fire({type: "SR_CRUD:NETWORK_PLAYER_UPDATED", player});
         expect(handler).to.have.been.called;
+        expect(store.dispatch).to.have.been.calledWith({
+            type: "SR_CRUD:UPDATE_PLAYER",
+            player
+        });
     });
 
     it("should listen for SR_CRUD:NETWORK_<schema>_DELETED", () => {
-        const component = new ClientCrud(schema);
         const handler = spy(component._eventListeners, "SR_CRUD:NETWORK_PLAYER_DELETED");
-        component.fire({type: "SR_CRUD:NETWORK_PLAYER_DELETED"});
+        component.fire({type: "SR_CRUD:NETWORK_PLAYER_DELETED", id: player.id});
         expect(handler).to.have.been.called;
+        expect(store.dispatch).to.have.been.calledWith({
+            type: "SR_CRUD:DELETE_PLAYER",
+            id: player.id
+        });
     });
 });
